766e536016d55f9ee7a68fd16a4e8f9167105db6
Arjit Jain <arjitjain2407@gmail.com>
2023-05-17 17:25:10 +0530

    Update random.py

--- Project/random.py
+++ Project/random.py
@@ -1,14 +1,14 @@
 from playsound import playsound
-import random
+import numpy as np
 
 def my_playlist():
 	my_list = []
 	while len(my_list)<=20:
-		number = random.randint(1, 20)
+		number = np.random.randint(1, 20)
 		if number not in my_list:
 			my_list.append(number)
 			number_str = str(number)
-			playsound(f'/songs/'+ number_str +'.mp3')
+			playsound('songs/'+ number_str +'.mp3')
 
 for times in range(0,5):
     my_playlist()
