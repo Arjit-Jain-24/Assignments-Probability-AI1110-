eef52d1fd12d3dfa5a92e752c723a3940e66c1b7
Arjit Jain <arjitjain2407@gmail.com>
2023-05-18 12:39:56 +0530

    Update random.py

--- Project/random.py
+++ Project/random.py
@@ -1,14 +1,45 @@
-from playsound import playsound
+import pygame
 import numpy as np
+import threading
+import time
+import os
+
+def play_song(song_number, stop_event):
+    number_str = str(song_number)
+    print("playing song", number_str)
+    song_path = os.path.join('songs', number_str + '.mp3')
+    pygame.mixer.music.load(song_path)
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy() and not stop_event.is_set():
+        continue
 
 def my_playlist():
-	my_list = []
-	while len(my_list)<=20:
-		number = np.random.randint(1, 20)
-		if number not in my_list:
-			my_list.append(number)
-			number_str = str(number)
-			playsound('songs/'+ number_str +'.mp3')
+    my_list = []
+    stop_event = threading.Event()
 
-for times in range(0,5):
+    pygame.mixer.init()
+
+    while len(my_list) <= 20:
+        number = np.random.randint(1, 20)
+        if number not in my_list:
+            my_list.append(number)
+
+            # Create a new thread to play the song
+            song_thread = threading.Thread(target=play_song, args=(number, stop_event))
+            song_thread.start()
+
+            # Prompt for next song in the main thread
+            user_input = input("Press 'n' for the next song, or any other key to stop: ")
+            if user_input.lower() != 'n':
+                stop_event.set()
+                break
+            else:
+                stop_event.set()
+                time.sleep(0.5)  # Wait for the current song to stop completely
+                stop_event.clear()
+
+    pygame.mixer.quit()
+
+while True:
     my_playlist()
